e9310e7d13cc1b26dbb0b08880294b307ed9fe4c
Ekas <ekaspreet99@gmail.com>
2023-06-02 03:59:38 +0530

    second commit

--- src/components/ShowSummary.jsx
+++ src/components/ShowSummary.jsx
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../App.css';
+// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import '../style.css';
 
 const ShowSummary = () => {
   const { showId } = useParams();
   const [summary, setSummary] = useState([]);
+  const [img, setImg] = useState("");
 
   useEffect(() => {
     const fetchSummary = async () => {
       try {
         const response = await axios.get(`https://api.tvmaze.com/shows/${showId}`);
         setSummary(response.data);
+        setImg(response.data.image.medium);
       } catch (error) {
         console.error('Error fetching show summary:', error);
       }
     };
 
     fetchSummary();
+    
   }, [showId]);
 
-  console.log(summary);
 
   return (
     <div>
@@ -30,36 +34,25 @@ const ShowSummary = () => {
       <h1 style={{ margin: "20px 50px", color:"white" }} >Show Summary</h1>
             </div>
             <div className="col" style={{ justifyContent: "center" }}>
-      <Link to="/"><button>Home</button></Link>
+      <Link to="/"><button className='home-btn' >Home</button></Link>
             </div>
         </div>
       <div className="summary-box">
         <h2>{ summary.name }</h2>
-        {/* <img  src={summary.image.medium} alt={summary.name} />
-        <p id="summary" ></p>
-        <div style={{display:"none"}}>
-            {document.getElementById("summary").innerHTML = summary.summary}
-        </div> */}
-        {/* <div className="row">
-            <div className="col"> */}
-                {/* <span id='rating'>⭐{ summary.rating.average }</span>
-                <img src={summary.image.medium} alt={summary.name} /> */}
-            {/* </div> */}
-            {/* <div className="col"> */}
-                 {/* <p className="summary" ></p>
-                <div style={{display:"none"}}>
-                    {document.getElementsByClassName("summary")[0].innerHTML = summary.summary}
-                </div> */}
-            {/* </div> */}
-        {/* </div> */}
-            <p>{summary.summary}</p>
-            <p><b>Language: </b>{summary.language}</p>
-            <p><b>Genre: </b>{summary.genres}</p>
-            <p><b>Runtime: </b>{summary.runtime}</p>
-            <p><b>Official Site: </b><a href={summary.officialSite}>{summary.officialSite}</a></p>
-            <p><b>Status: </b>{summary.status}</p>
-
-        <button>Book a Movie Ticket</button>
+        <div className="row">
+            <div className="col">
+                <img  src={img} alt={summary.name} />
+                <button className='book-show' >Book a Movie Ticket</button>
+            </div>
+            <div className="col movie-summary">
+              <div  className="gray" dangerouslySetInnerHTML={{__html: summary.summary}}></div>    
+              <p><b>Language: </b><span className="gray">{summary.language}</span></p>
+              <p><b>Genre: </b><span className="gray">{summary.genres}</span></p>
+              <p><b>Runtime: </b><span className="gray">{summary.runtime}</span></p>
+              <p><b>Official Site: </b><span className="gray"><a href={summary.officialSite}>{summary.officialSite}</a></span></p>
+              <p><b>Status: </b><span className="gray">{summary.status}</span></p>
+            </div>
+        </div>
       </div>
     </div>
   );
